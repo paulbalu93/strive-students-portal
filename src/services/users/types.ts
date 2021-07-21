7229cda417c4a787a246f590961037c95047f50f
Paul Maliakel <paulbalu93@gmail.com>
2021-07-21 10:41:11 +0200

    adds types

--- src/services/users/types.ts
+++ src/services/users/types.ts
@@ -1,26 +1,28 @@
 import { Document, Model} from 'mongoose'
 export interface User{
 
-    email: string 
-    password: string
-    firstName?: string
-    lastName?: string
-    profilePic?: string
-    githubURl?: string
-    portfolioURL?: string
-    capstoneURL?: string
-    linkedinURL?: string
-    youtubeURL?: string
-    availableToRelocation?: boolean
-    located?: string
-    desiredPosition?: string
-    approved: boolean
-    hired?: boolean
-    track: string
-    oneLiner?: string
+    email: string,
+    password: string,
+    firstName?: string,
+    lastName?: string,
+    profilePic?: string,
+    githubURl?: string,
+    portfolioURL?: string,
+    capstoneURL?: string,
+    linkedinURL?: string,
+    youtubeURL?: string,
+    availableToRelocation?: boolean,
+    located?: string,
+    desiredPosition?: string,
+    approved: boolean,
+    hired?: boolean,
+    track: string,
+    oneLiner?: string,
 }
 
 export interface UserDocument extends User, Document {}
 
-export interface UserModel extends Model<UserDocument> {}
+export interface UserModel extends Model<UserDocument>{}
+    // checkCredentials(email: string, password: string): Promise<UserDocument| null>
+
 
